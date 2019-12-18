5fe25e8ea608062784e96ee2571dd1ba5f4a629c
Luis Puente <puente.luism@gmail.com>
2019-12-18 22:53:40 +0000

    Add user models

---
+++ test/models/comment_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class CommentTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  
+#  test "it works" do
+ #   assert true, comment.save
+  #end
+end
